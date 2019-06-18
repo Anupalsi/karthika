210ae69140558d2851923ab746319fd40186cdcd
Anupalsi <46413796+Anupalsi@users.noreply.github.com>
2019-06-18 11:22:58 +0530

    Create play26.py

---
+++ play26.py
@@ -0,0 +1,8 @@
+#anu
+s=list(map(str,input()))
+for i in range(len(s)):
+  if s[i].islower()==True:
+        s[i]=s[i].capitalize()
+  else:
+        s[i]=s[i].lower()
+for i in range(len(s)): print(s[i],end="")

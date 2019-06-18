2c6e2f0a2b33ce252bfb0b71ec8ae912c66b3e4d
Anupalsi <46413796+Anupalsi@users.noreply.github.com>
2019-06-18 15:54:54 +0530

    Create pro31.py

---
+++ pro31.py
@@ -0,0 +1,16 @@
+#anu  
+a1,b1=map(int,input().split())
+if a1<=b1:
+  d=a1
+else:
+  d=b1
+z=[]
+for i in range(0,d):
+  z.append(sorted(list(map(int,input().split()))))
+z=sorted(z)
+for i in range(0,len(z[0])):
+  for j in range(0,len(z)-1):
+    if z[j][i]>z[j+1][i]:
+      z[j][i],z[j+1][i]=z[j+1][i],z[j][i]
+for i in z:
+  print(*i)
